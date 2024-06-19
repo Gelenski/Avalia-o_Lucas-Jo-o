c3d131d2002befd387636f0e29f460990d27e730
Lucas Gelenski <115673131+Gelenski@users.noreply.github.com>
2024-06-19 07:45:47 -0300

    Atualizar o app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 from modelos.restaurante import Restaurante
 
 
-class ProgramaExpresso:
+class Programa:
     def __init__(self):
         self.restaurantes = [
             Restaurante("Veneza", "Comida Italiana"),
@@ -132,5 +132,5 @@ class ProgramaExpresso:
 
 
 if __name__ == "__main__":
-    programa = ProgramaExpresso()
+    programa = Programa()
     programa.main()
